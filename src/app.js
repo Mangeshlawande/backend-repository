fa1473970ac30528a7a98d78f02f72eccd3eb051
Mangeshlawande <mangeshlawande223@gmail.com>
2025-08-30 18:42:01 +0530

    import routes in app.js

--- src/app.js
+++ src/app.js
@@ -21,17 +21,31 @@ app.use(express.urlencoded({
 app.use(express.static("public"));
 app.use(cookieParser());
 
-
-app.use(errorHandler); // This must be the last middleware
-
-
 // routes import
 import userRouter from './routes/user.routes.js';
+import healthCheckRouter from './routes/healthcheck.routes.js';
+import tweetRouter from './routes/tweet.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 
 // routes declaration 
+app.use('/api/v1/healthcheck',healthCheckRouter);
 app.use('/api/v1/users',userRouter);
+app.use('/api/v1/tweets',tweetRouter);
+app.use('/api/v1/subscriptions',subscriptionRouter);
+app.use('/api/v1/videos',videoRouter);
+app.use('/api/v1/comments',commentRouter);
+app.use('/api/v1/likes',likeRouter);
+app.use('/api/v1/playlists',playlistRouter);
+app.use('/api/v1/dashboard',dashboardRouter);
+
 
 // http://localhost:8000/api/v1/users/register
 
+app.use(errorHandler); // This must be the last middleware
 export { app }
